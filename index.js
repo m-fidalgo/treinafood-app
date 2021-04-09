333d8724ce43fda4467725c302a94d8f840b21ac
Maira Fidalgo <mairaberlatof@gmail.com>
2021-04-09 12:22:31 -0300

    providers

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import { AppRegistry } from "react-native";
 import App from "./app/App";
 import { name as appName } from "./app.json";
+import ProductProvider from "./app/ui/providers/ProductProvider";
+import CartProvider from "./app/ui/providers/CartProvider";
 import { Provider as PaperProvider } from "react-native-paper";
 import { theme } from "./app/ui/styles/theme";
 
 function Index() {
   return (
     <PaperProvider theme={theme}>
-      <App />
+      <ProductProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </ProductProvider>
     </PaperProvider>
   );
 }
